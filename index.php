70bd0076880b201986f2035dc520ef4d1209be9b
bisrailov <69085276+bisrailov@users.noreply.github.com>
2021-05-05 16:16:08 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 echo "<h2>pdftk</h2>";
-echo "<pre>"
+echo "<pre>";
 exec('pdftk', $output);
 foreach($output as $row){
     echo $row;
 }
 echo "</pre>";
 echo "<h2>prince</h2>";
-echo "<pre>"
+echo "<pre>";
 exec('prince', $output);
 foreach($output as $row){
     echo $row;
