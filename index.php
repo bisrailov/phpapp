5f3fb0212218e3b949b457818cf83a735318b695
bisrailov <69085276+bisrailov@users.noreply.github.com>
2021-05-05 16:05:06 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+echo "<h2>pdftk</h2>";
+echo "<pre>"
+exec('pdftk', $output);
+foreach($output as $row){
+    echo $row;
+}
+echo "</pre>";
+echo "<h2>prince</h2>";
+echo "<pre>"
+exec('prince', $output);
+foreach($output as $row){
+    echo $row;
+}
+echo "</pre>";
