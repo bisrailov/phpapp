bb6f2f75a77e9c251ee5756aa7c8e87fc76753df
bisrailov <69085276+bisrailov@users.noreply.github.com>
2021-05-18 15:53:10 -0500

    phpinfo

--- index.php
+++ index.php
@@ -1,15 +1,1 @@
-<?php
-echo "<h2>pdftk</h2>";
-echo "<pre>";
-exec('pdftk', $output);
-foreach($output as $row){
-    echo $row;
-}
-echo "</pre>";
-echo "<h2>prince</h2>";
-echo "<pre>";
-exec('prince', $output);
-foreach($output as $row){
-    echo $row;
-}
-echo "</pre>";
+<?php phpinfo();
