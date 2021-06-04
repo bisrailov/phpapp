b358a83f014e80f517728399dce443e4088cf627
bisrailov <69085276+bisrailov@users.noreply.github.com>
2021-06-04 12:43:38 -0500

    redis check

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-<?php phpinfo();
+<?php
+echo "</pre>";
+echo "testing redis\n";
+try {
+    $redis = new Redis();
+    $ret   = $redis->connect('localhost');
+    if ($ret) {
+        echo "redis connected\n";
+        $ret = $redis->ping('test');
+        echo "ping success\n";
+        $redis->set('testkey', 'testvalue');
+        echo "set\n";
+        $ret = $redis->get('testkey');
+        if ($ret == 'testvalue') {
+            echo "get success\n";
+        } else {
+            echo "get failed\n";
+        }
+    }
+} catch (RedisException $re) {
+    echo "redis exception: " . $re->getMessage() . "\n";
+}
+echo "</pre>";
+phpinfo();
